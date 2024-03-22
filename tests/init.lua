a6bd41c0dbdd0e4972d6f0aa9f21f4f0e540cda2
Sina Siadat <siadat@gmail.com>
2024-03-22 22:12:40 +0000

    decided to add some stuff just for when I do asciinema recordings, I might remove these later

--- tests/init.lua
+++ tests/init.lua
@@ -18,9 +18,24 @@ vim.opt.rtp:prepend(lazypath)
 -- Install Shell.nvim
 --
 require("lazy").setup({
-  {
-    "siadat/shell.nvim",
-    opts = {},
-    dev = true,
-  },
+  { "siadat/shell.nvim", opts = {}, dev = true },
+  { "siadat/animated-resize.nvim", opts = {} }, -- For asciinema recording
 }, { dev = { path = '/work/src/nvim-plugins' } })
+
+--
+-- My minimal personal configuration
+--
+vim.o.mouse = ''
+vim.wo.wrap = false
+vim.keymap.set('n', '<c-f>', '<esc>', { desc = 'sina: escape' })
+vim.keymap.set('i', '<c-f>', '<esc>', { desc = 'sina: escape' })
+vim.keymap.set('v', '<c-f>', '<esc>', { desc = 'sina: escape' })
+vim.keymap.set('c', '<c-f>', '<esc>', { desc = 'Sina: escape' })
+vim.keymap.set('t', '<c-f>', '<esc>', { desc = 'Sina: escape' })
+vim.keymap.set('s', '<c-f>', '<esc>', { desc = 'Sina: escape' })
+vim.keymap.set('n', '<c-j>', '<c-w>j', { desc = 'Sina: navigating windows' })
+vim.keymap.set('n', '<c-k>', '<c-w>k', { desc = 'Sina: navigating windows' })
+vim.keymap.set('n', '<c-h>', '<c-w>h', { desc = 'Sina: navigating windows' })
+vim.keymap.set('n', '<c-l>', '<c-w>l', { desc = 'Sina: navigating windows' })
+vim.keymap.set('n', ';w', ':up<cr>', { desc = 'Sina: write/update buffer' })
+vim.keymap.set('n', ';q', ':q<cr>', { desc = 'Sina: close window' })
